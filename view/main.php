7f93fa018d83768806156e64bd13787b04f275e6
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2018-06-30 12:13:57 +0700

    update - add file

---
+++ view/main.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Chupy</title>
+  <link rel="stylesheet" href="/extension/plugins/bootstrap-4.1.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/extension/css/chupy-style.css">
+</head>
+<body>
+
+    <?php include('template/navbar.php');?>
+
+    <main>
+      
+    </main>
+
+    <?php include('template/footer.php');?>
+
+</body>
+</html>
---
+++ view/template/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lgnavbar navbar-expand-lg">
+  <a href="#" class="navbar-brand"> <img src="/extension/img/chupy_icon-light.png" alt="Chupy Brand"> </a>
+
+  <div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav nav justify-content-end">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Produk</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Masuk/Daftar</a>
+    </li>
+  </ul>
+</div>
+</nav>
