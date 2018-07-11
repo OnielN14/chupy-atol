3b5acdc7063a67d5961e80f384921e1ff5261f1f
Maulvi Inayat Ali <maulvi67@gmail.com>
2018-07-11 10:28:42 +0700

    add - barang lainnya

--- view/detail_barang.php
+++ view/detail_barang.php
@@ -34,35 +34,81 @@ include('template/navbar.php');
                     </div>
                 </section>
         </header>
-        <div class="container-fluid">
-            <section class="my-5">
-                <div class="row ml-2">
-                    <div class="col-md-4">
-                        <img src="/extension/img/chupy-box-ATOL.png" alt="Foto Detail Barang" class="img-detail-barang float-center">
-                    </div>
 
-                    <div class="col-md-4">
-                        <h4>Rp.696969696</h4>
-                        <h5>Deskripsi Produk</h5>
-                        <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, fugit corporis! Ipsa aperiam enim
-                            nobis possimus quo quisquam, porro tenetur illum temporibus velit. In autem accusamus impedit
-                            praesentium pariatur similique.</h6>
-
-                    </div>
-
-                    <div class="col-md-4">
-                        <div class="btn-group-vertical w-75">
-                            <div class="btn btn-primary mb-3 w-100">Tambah Ke Keranjang</div>
-                            <div class="btn btn-primary outline  mb-3 w-100 ">Tambah Ke Wishlist</div>
-                        </div>
-                        <h5>Stok : 69</h5>
-
-                    </div>
+        <section class="mt-3 ">
+            <div class="row ml-2">
+                <div class="col-md-4">
+                    <img src="/extension/img/chupy-box-ATOL.png" alt="Foto Detail Barang" class="img-detail-barang float-center">
                 </div>
-            </section>
 
-            
-        </div>
+                <div class="col-md-4">
+                    <h4>Rp.696969696</h4>
+                    <h5>Deskripsi Produk</h5>
+                    <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, fugit corporis! Ipsa aperiam enim nobis
+                        possimus quo quisquam, porro tenetur illum temporibus velit. In autem accusamus impedit praesentium
+                        pariatur similique.</h6>
+
+                </div>
+
+                <div class="col-md-4">
+                    <div class="btn-group-vertical w-75">
+                        <div class="btn btn-primary mb-3 w-100">Tambah Ke Keranjang</div>
+                        <div class="btn btn-primary outline  mb-3 w-100 ">Tambah Ke Wishlist</div>
+                    </div>
+                    <h5>Stok : 69</h5>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="chupy-detail-barang">
+            <hr class="mx-3">
+            <h5 class="mx-3">Barang Lainnya</h5>
+            <div class="row ml-2">
+                <article class=" col-md-3 chupy-detail-card">
+                    <a href="/" class="card">
+                        <img src="/extension/img/chupy-box-ATOL.png" alt="box ATOL" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Rp. 69696969</p>
+                        </div>
+                    </a>
+                </article>
+
+                <article class=" col-md-3 chupy-detail-card">
+                    <a href="/" class="card">
+                        <img src="/extension/img/chupy-box-ATOL.png" alt="box ATOL" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Rp. 69696969</p>
+                        </div>
+                    </a>
+                </article>
+
+                <article class=" col-md-3 chupy-detail-card">
+                    <a href="/" class="card">
+                        <img src="/extension/img/chupy-box-ATOL.png" alt="box ATOL" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Rp. 69696969</p>
+                        </div>
+                    </a>
+                </article>
+
+                <article class=" col-md-3 chupy-detail-card">
+                    <a href="/" class="card">
+                        <img src="/extension/img/chupy-box-ATOL.png" alt="box ATOL" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Rp. 69696969</p>
+                        </div>
+                    </a>
+                </article>
+
+
+            </div>
+        </section>
+
     </main>
 
     <?php
